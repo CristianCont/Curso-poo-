99ae2f5285efa11871e85d3503864b2599b7abff
cristian camilo contreras valbuena <Cristian.99145@gmail.com>
2022-12-28 17:25:03 -0500

    Creacion deobjetos en el index de php

--- PHP/index.php
+++ PHP/index.php
@@ -1,7 +1,12 @@
 <?php
 require_once('Car.php');
 require_once('Account.php');
+require_once('UberX.php');
+require_once('UberPool.php');
 
-$car=new Car("Asd455", new Account ("Marywxi","1231648979"));
-$car->PrintDataCar();
+$uberX=new UberX("srg789", new Account("Andres Matilla","4974949"),"Chevrolet", "Spark gt");
+$uberX->PrintDataCar();
+
+$uberPool=new UberPool("164gtr",new Account("Camilo Sexto","164984949"),"Chevrolet","Corsa");
+$uberPool->PrintDataCar();
 ?>
